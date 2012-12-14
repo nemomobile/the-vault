8f1bc4f5efb0abb351a673bfecd04d1ed3cdc0ee
Denis Zalevskiy <denis.zalevskiy@jollamobile.com>
2012-12-14 18:51:11 +0200

    --action option
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jollamobile.com>

--- examples/scripts/contacts.py
+++ examples/scripts/contacts.py
@@ -8,9 +8,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--dir', dest='data', type=str, help='destination dir for text files')
 parser.add_argument('-b', '--bin-dir', dest='bin', type=str, help='destination dir for blobs')
 parser.add_argument('-H', '--home-dir', dest='home', type=str, help='home dir')
+parser.add_argument('-a', '--action', dest='action',
+                    help='Action to be performed: import, export, clear')
+
 args = parser.parse_args()
-print os.getcwd(), args.home
-src = os.path.join(args.home, 'vcf', 'out', '.')
-print "From:", src
-shutil.copytree(src, args.data)
+
+if args.action == 'export':
+    print os.getcwd(), args.home
+    src = os.path.join(args.home, 'vcf', 'out', '.')
+    print "From:", src
+    shutil.copytree(src, args.data)
+else:
+    print "Action {} is not implemented".format(args.action)
+    exit(1)
 
--- examples/scripts/openclipart.py
+++ examples/scripts/openclipart.py
@@ -8,12 +8,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--dir', dest='data', type=str, help='destination dir for text files')
 parser.add_argument('-b', '--bin-dir', dest='bin', type=str, help='destination dir for blobs')
 parser.add_argument('-H', '--home-dir', dest='home', type=str, help='home dir')
+parser.add_argument('-a', '--action', dest='action',
+                    help='Action to be performed: import, export, clear')
+
+
 args = parser.parse_args()
-print os.getcwd(), args.home
-src = os.path.join(args.home, 'openclipart', 'text', '.')
-bin_src = os.path.join(args.home, 'openclipart', 'bin', '.')
-print "From:", src
-shutil.copytree(src, args.data)
-print "From:", bin_src
-shutil.copytree(bin_src, args.bin)
+
+if args.action == 'export':
+    src = os.path.join(args.home, 'openclipart', 'text', '.')
+    bin_src = os.path.join(args.home, 'openclipart', 'bin', '.')
+    print "From:", src
+    shutil.copytree(src, args.data)
+    print "From:", bin_src
+    shutil.copytree(bin_src, args.bin)
+else:
+    print "Action {} is not implemented".format(args.action)
+    exit(1)
 
