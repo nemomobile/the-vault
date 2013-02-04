3acd18168b140f092aab3f4ecb0a2d39118a1a76
Denis Zalevskiy <denis.zalevskiy@jollamobile.com>
2013-02-05 00:54:23 +0200

    example: pictures
    just shows it is possible to backup relatively big binary files but it
    is not recommended because it requires 2-3x times more persistent
    storage space that total size of images
    
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jollamobile.com>

---
+++ examples/scripts/pictures.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env qtscript
+
+qtscript.use("qt.core")
+qtscript.load("util.js")
+qtscript.load("sys.js")
+qtscript.load("os.js")
+
+print("Pictures backup: ", qtscript.script.args)
+
+var cmdline = lib.sys.getopt({
+    data_dir : { short_ : "d", long_ : "dir",
+                 required : true, has_param : true},
+    bin_dir : { short_ : "b", long_ : "bin-dir",
+                required : true, has_param : true},
+    home : { short_ : "H", long_ : "home-dir",
+             required : true, has_param : true},
+    action : { short_ : "a", long_ : "action",
+               required : true, has_param : true}
+}).parse(qtscript.script.args)
+
+var options = cmdline.opts
+var action = options.action;
+var src, dst
+var os = lib.os
+
+switch (action) {
+case 'export':
+    print("EXPORT")
+    src = lib.os.path(options.home, 'Pictures')
+    os.cptree(src, options.bin_dir)
+    break;
+case 'import':
+    print("IMPORT")
+    dst = lib.os.path(options.home, 'Pictures')
+    os.cptree(options.bin_dir, dst)
+    break;
+default:
+    throw lib.error({ msg : "Unknown action", action : action});
+    break;
+}
